59e03b24c1d0fe654a8aab980a7fee13df1d87d0
Tim_redBlack <67490655+timoluxinne@users.noreply.github.com>
2020-11-17 09:10:53 +0300

    Update settings.py

--- Blog_2/settings.py
+++ Blog_2/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='timoluxinne@gmail.com'
-EMAIL_HOST_PASSWORD = 'cwmsxpstlehfiabv'
+EMAIL_HOST_USER =''
+EMAIL_HOST_PASSWORD = ''
